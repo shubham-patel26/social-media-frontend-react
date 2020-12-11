230bfb88931e1794df896c6a0d3634c7171ee9a9
shubham <shubham763116@gmail.com>
2020-12-12 01:41:50 +0530

    voting done

--- src/Components/DisplayPost.js
+++ src/Components/DisplayPost.js
@@ -1,7 +1,7 @@
 import React , {Component} from 'react';
 import Axios from 'axios';
 import { Collapse, Button, CardBody,Card,Form,  FormGroup, Input  } from 'reactstrap';
-
+import {Link } from 'react-router-dom'
 
 import DisplayComment from './DisplayComment';
 class DisplayPost extends Component{
@@ -9,6 +9,7 @@ class DisplayPost extends Component{
         super(props);
         this.toggle = this.toggle.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleVoting = this.handleVoting.bind(this);
         this.state={
             post:'',
             comments: [],
@@ -66,7 +67,7 @@ class DisplayPost extends Component{
             }
         })
         .then(resp=>{
-            // console.log(resp);
+            console.log(resp);
             this.setState({
                 comments: resp.data
             })
@@ -74,24 +75,33 @@ class DisplayPost extends Component{
         .catch(err=>console.log(err));
         alert("comment: " + this.comment.value );
         event.preventDefault();
+        event.target.reset();
 
+    }
+
+    handleVoting(type){
+        const bearer = 'Bearer ' + localStorage.getItem('token');
+        let api = type==1?`http://localhost:3444/upvote/${this.state.post.postId}`:`http://localhost:3444/downvote/${this.state.post.postId}`;
+        Axios.get(api,{
+            headers:{
+                'authorization': bearer,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(resp=>{
+            // console.log(resp);
+            this.setState({
+                post: resp.data
+            })
+        })
+        .catch(err=>console.log(err));
     }
 
     render(){
         // console.log(this.state.author);
         return (
             <div className="container">
-                {/* <div className='row'>
-                    <div className='col-md-2'>
-                    <button>Like</button>
-                    </div>
-
-                    <div className='col-md'> */}
-                        {/* <h3>{this.state.post.heading}</h3>
-                        <p>
-                            {this.state.post.body}
-                        </p> */}
-                        
+                
                         <div className="row  ">
                             <div className="col-12 col-md-8 col-xl-9 ">
                                 <div className="card">
@@ -100,8 +110,44 @@ class DisplayPost extends Component{
                                 <p className="writing-time">- {this.state.post.postedOn}</p>
                                     <p className="card-text tt1">{this.state.post.body} </p>
                                 </div>
+                               
                                 </div>
-                                
+                                <div className="row">
+                
+                <div className='col-1'>
+                    <Link>
+                    {this.state.post.hasUserUpvoted?
+                    <i onClick={()=>this.handleVoting(2)} className="fa fa-heart" style={{fontSize:'41px',color:'red'}}/>:
+                    <i onClick={()=>this.handleVoting(1)} className="fa fa-heart-o" style={{fontSize:'41px',color:'red'}}/>
+                    }
+                    </Link>
+                    
+                        
+                        
+                    </div>
+                    <div className='col-11'>
+                        
+                        <Form className='row' onSubmit={this.handleSubmit}>
+                        <div className='col-1'><Button type="submit" value="submit" color="primary" >post</Button></div>
+                        <div className='col-11'>
+                        <FormGroup >
+                            
+                            <Input  type="text" id="comment" name="comment" placeholder="post your comment"
+                                innerRef={(input) => this.comment = input} />
+                        </FormGroup>
+                        </div>
+                        
+                            
+
+                            
+                        </Form>
+                        
+                    
+                    </div>
+                        
+                
+            
+            </div>
                             </div>
                             <div className="col-12 col-md-4 col-xl-3 side">
                                 <div className="card mb-5">
@@ -121,24 +167,7 @@ class DisplayPost extends Component{
                         </div>
                     
 
-                <div className="row">
-                <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
-                    <Collapse isOpen={this.state.isOpen}>
-                        <Card>
-                        <CardBody>
-                                <Form onSubmit={this.handleSubmit}>
-                                    <FormGroup>
-                                        
-                                        <Input type="text" id="comment" name="comment" placeholder="post your comment"
-                                            innerRef={(input) => this.comment = input} />
-                                    </FormGroup>
-                                    
-                                    <Button type="submit" value="submit" color="primary">post</Button>
-                                </Form>
-                        </CardBody>
-                        </Card>
-                    </Collapse>
-                </div>
+                       
 
 
                 <div className='row'>
